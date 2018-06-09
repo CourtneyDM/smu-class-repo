3c85531282d40031afd94daba27b673a90deddde
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-06-09 14:05:00 -0500

    React in class activities

---
+++ activities/week-19-react/02-HelloReact/Unsolved/reactpractice/src/components/HelloReact.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const HelloReact = () => <p>Hello, from the HelloReact.js File!</p>;
+
+export default HelloReact;
---
+++ activities/week-19-react/03-HelloDiv/hellodiv/src/components/HelloDiv.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HelloDiv = () => {
+    return (
+
+        <React.Fragment>
+            <h3>Hello, My name is Courtney</h3>
+            <ul>
+                <li>React</li>
+                <li>Pizza</li>
+                <li>Beer</li>
+            </ul>
+        </React.Fragment>
+    )
+};
+
+export default HelloDiv;
