09592a3b180538108cce80df3e141f113c7f1c12
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-06-16 14:15:21 -0500

    In class React activities

--- class/w20-react/08-Stu_Recipes/Unsolved/client/src/App.js
+++ class/w20-react/08-Stu_Recipes/Unsolved/client/src/App.js
@@ -6,75 +6,84 @@ import Button from "./components/Button";
 import API from "./utils/API";
 import { RecipeList, RecipeListItem } from "./components/RecipeList";
 import { Container, Row, Col } from "./components/Grid";
+import Thumbnail from "./components/Thumbnail";
 
 class App extends Component {
-  state = {
-    recipes: [],
-    recipeSearch: ""
-  };
+    state = {
+        recipes: [],
+        recipeSearch: ""
+    };
 
-  handleInputChange = event => {
-    // Destructure the name and value properties off of event.target
-    // Update the appropriate state
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
-  };
+    handleInputChange = event => {
+        // Destructure the name and value properties off of event.target
+        // Update the appropriate state
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
 
-  handleFormSubmit = event => {
-    // When the form is submitted, prevent its default behavior, get recipes update the recipes state
-    event.preventDefault();
-    API.getRecipes(this.state.recipeSearch)
-      .then(res => {
-        console.log(res.data);
-        this.setState({ recipes: res.data });
-      })
-      .catch(err => console.log(err));
-  };
+    handleFormSubmit = event => {
+        // When the form is submitted, prevent its default behavior, get recipes update the recipes state
+        event.preventDefault();
+        API.getRecipes(this.state.recipeSearch)
+            .then(res => {
+                console.log(res.data);
+                this.setState({ recipes: res.data });
+            })
+            .catch(err => console.log(err));
+    };
 
-  render() {
-    return (
-      <div>
-        <Nav />
-        <Jumbotron />
-        <Container>
-          <Row>
-            <Col size="md-12">
-              <form>
+    render() {
+        return (
+            <div>
+                <Nav />
+                <Jumbotron />
                 <Container>
-                  <Row>
-                    <Col size="xs-9 sm-10">
-                      <Input
-                        name="recipeSearch"
-                        value={this.state.recipeSearch}
-                        onChange={this.handleInputChange}
-                        placeholder="Search For a Recipe"
-                      />
-                    </Col>
-                    <Col size="xs-3 sm-2">
-                      <Button
-                        onClick={this.handleFormSubmit}
-                        type="success"
-                        className="input-lg"
-                      >
-                        Search
-                      </Button>
-                    </Col>
-                  </Row>
+                    <Row>
+                        <Col size="md-12">
+                            <form>
+                                <Container>
+                                    <Row>
+                                        <Col size="xs-9 sm-10">
+                                            <Input
+                                                name="recipeSearch"
+                                                value={this.state.recipeSearch}
+                                                onChange={this.handleInputChange}
+                                                placeholder="Search For a Recipe"
+                                            />
+                                        </Col>
+                                        <Col size="xs-3 sm-2">
+                                            <Button onClick={this.handleFormSubmit} type="success" className="input-lg">Search</Button>
+                                        </Col>
+                                    </Row>
+                                </Container>
+                            </form>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col size="xs-12">
+                            <h1>
+                                <RecipeList>
+                                    {this.state.recipes.map((recipe, i) => {
+                                        return (
+                                            <RecipeListItem
+                                                key={i}
+                                                title={recipe.title}
+                                                href={recipe.href}
+                                                ingredients={recipe.ingredients}
+                                                thumbnail={recipe.thumbnail}
+                                            />
+                                        );
+                                    })}
+                                </RecipeList>
+                            </h1>
+                        </Col>
+                    </Row>
                 </Container>
-              </form>
-            </Col>
-          </Row>
-          <Row>
-            <Col size="xs-12">
-              <h1>Render Recipes Here</h1>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 
 export default App;
