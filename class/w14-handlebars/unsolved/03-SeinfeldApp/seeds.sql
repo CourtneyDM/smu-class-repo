03346db10798088faed0666559a83cadcc836766
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-06-18 11:14:43 -0500

    Removed duplicate files

--- class/w14-handlebars/unsolved/03-SeinfeldApp/seeds.sql
+++
@@ -1,3 +0,0 @@
-USE seinfeld_db;
-INSERT INTO actors (char_name, coolness_points, attitude)
-VALUES ('Jerry Seinfeld', 7, 'calm'), ('Elaine Benes', 5, 'edgy'), ('George Costanza', 6, 'humorous'), ('Kramer', 4, 'dingy');
