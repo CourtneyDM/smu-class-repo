e345af773a1f6d2e0c6a862a8879d3cb0c670b82
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-06-19 23:50:45 -0500

    Imported solution files

--- class/w20-react/08-Stu_Recipes/Unsolved/server.js
+++ class/w20-react/08-Stu_Recipes/Unsolved/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+  app.use(express.static("client/build"));
 }
 
 // Use apiRoutes
@@ -18,10 +18,10 @@ app.use("/api", apiRoutes);
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function () {
-    console.log(`🌎 ==> Server now on port ${PORT}!`);
+app.listen(PORT, function() {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
